e09522a4ffeb48c0f180a08a8646097f8d60b64c
otaneto <otaciliolucas@gmail.com>
2020-08-16 12:22:47 -0300

    feature: add vue-the-mask package

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue';
+import VueTheMask from 'vue-the-mask';
+
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false;
+Vue.use(VueTheMask);
 
 new Vue({
   router,
