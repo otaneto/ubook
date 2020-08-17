0463189036d95bf1b779d1e80da5c5fa4244cb1a
otaneto <otaciliolucas@gmail.com>
2020-08-17 18:09:28 -0300

    feature: add new colors

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -12,6 +12,9 @@ export default new Vuetify({
         'light-yellowish-green': colors.lightYellowishGreen,
         salmon: colors.salmon,
         'cloudy-blue': colors.cloudyBlue,
+        'blue-grey': colors.blueGrey,
+        'very-light-pink': colors.veryLightPink,
+        'white-two': colors.whiteTwo,
       },
     },
     options: {
