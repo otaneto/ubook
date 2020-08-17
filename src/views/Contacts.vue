0736813ff71b3ac47fad7abb1efdb23124358689
otaneto <otaciliolucas@gmail.com>
2020-08-17 18:05:58 -0300

    refactor: rename Home view to Contacts

---
+++ src/views/Contacts.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="contacts-container">
+    <contacts-table v-if="contacts.length" />
+    <empty-content v-else />
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+import ContactsTable from '../components/ContactsTable.vue';
+import EmptyContent from '../components/EmptyContent.vue';
+
+export default {
+  name: 'Contacts',
+  components: {
+    ContactsTable,
+    EmptyContent,
+  },
+  computed: {
+    ...mapGetters(['contacts']),
+  },
+};
+</script>
+
+<style lang="scss">
+  .contacts-container {
+    height: 100%;
+  }
+</style>
