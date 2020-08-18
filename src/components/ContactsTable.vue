7ce5e9d641f42773bb98c520d91acd0448363424
otaneto <otaciliolucas@gmail.com>
2020-08-17 21:09:52 -0300

    featuer: create and add EditContactModal

--- src/components/ContactsTable.vue
+++ src/components/ContactsTable.vue
@@ -1,31 +1,59 @@
 <template>
+  <div>
     <v-data-table
-    :headers="headers"
-    :items="contacts"
-    :items-per-page="5"
-    class="elevation-0 contacts-table"
-    no-data-text="Nenhum contato foi criado ainda."
-    dense
-    hide-default-footer
-    disable-pagination
-    fixed-header
-  >
-    <template v-slot:item="{ item }">
-      <tr :id="item.id">
-        <td>{{ item.name || 'Não informado' }}</td>
-        <td>{{ item.email || 'Não informado' }}</td>
-        <td>{{ item.telephone || 'Não informado' }}</td>
-      </tr>
-    </template>
-  </v-data-table>
+      :headers="headers"
+      :items="contacts"
+      :items-per-page="5"
+      class="elevation-0 contacts-table"
+      no-data-text="Nenhum contato foi criado ainda."
+      dense
+      hide-default-footer
+      disable-pagination
+      fixed-header
+    >
+      <template v-slot:item="{ item }">
+        <tr :id="item.id">
+          <td>{{ item.name || 'Não informado' }}</td>
+          <td>{{ item.email || 'Não informado' }}</td>
+          <td>{{ item.telephone || 'Não informado' }}</td>
+          <td>
+            <div class="d-flex justify-end align-center">
+               <v-btn
+                class="mr-3"
+                icon
+                small
+                @click="toggleEditContactModal(item)"
+              >
+                <v-img contain src="@/assets/ic-edit.svg" width="16px" height="16px" />
+              </v-btn>
+              <v-btn icon small>
+                <v-img src="@/assets/ic-delete.svg" contain width="16" height="16" />
+              </v-btn>
+            </div>
+          </td>
+        </tr>
+      </template>
+    </v-data-table>
+    <edit-contact-modal
+      :isVisible="isEditModalContactVisible"
+      :selectedContact="item"
+      @cancel="isEditModalContactVisible = false"
+      @close="isEditModalContactVisible = false"
+    />
+  </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
+import { mapGetters, mapMutations } from 'vuex';
+
+import EditContactModal from './EditContactModal.vue';
 import colors from '../constants/colors';
 
 export default {
   name: 'ContactsTable',
+  components: {
+    EditContactModal,
+  },
   data() {
     return {
       headers: [
@@ -37,8 +65,17 @@ export default {
         },
         { text: 'E-mail', value: 'email' },
         { text: 'Telefone', value: 'telephone' },
+        { text: '', value: '', align: 'end' },
       ],
+      isEditModalContactVisible: false,
     };
+  },
+  methods: {
+    ...mapMutations(['SELECT_CONTACT']),
+    toggleEditContactModal(contact) {
+      this.SELECT_CONTACT(contact);
+      this.isEditModalContactVisible = true;
+    },
   },
   computed: {
     ...mapGetters(['contacts', 'newContact']),
@@ -46,13 +83,14 @@ export default {
   watch: {
     newContact(val) {
       if (val.id) {
+        console.log(val.id);
         setTimeout(() => {
-          this.$vuetify.goTo(`#${val.id}`, { duration: 1000, easing: 'easeInOutCubic' });
           document.getElementById(val.id).style.background = colors.veryLightPink;
-        }, 1);
+          this.$vuetify.goTo(`#${val.id}`, { duration: 1000, easing: 'easeInOutCubic' });
+        }, 700);
         setTimeout(() => {
           document.getElementById(val.id).style.background = 'white';
-        }, 10001);
+        }, 10700);
       }
     },
   },
@@ -64,6 +102,7 @@ export default {
     border: 1px solid $white-two;
   }
   .v-data-table .v-data-table__wrapper table tbody tr {
+    cursor: pointer;
     transition: background .5s ease-in-out;
   }
 
---
+++ src/components/EditContactModal.vue
@@ -0,0 +1,118 @@
+<template>
+  <v-dialog v-model="isVisible" width="432px" persistent>
+    <v-card width="432px">
+      <v-card-title>Editar contato</v-card-title>
+      <v-divider />
+      <v-card-text>
+        <v-form
+          v-model="valid"
+          ref="form"
+          class="contact-form"
+        >
+          <label>
+            Nome
+            <v-text-field v-model="contact.name" outlined dense />
+          </label>
+          <label>
+            E-mail
+            <v-text-field v-model="contact.email" outlined dense />
+          </label>
+          <label>
+            Telefone
+            <div class="telephone-field">
+              <v-text-field v-model="contact.telephone" outlined dense v-mask="telephoneMask" />
+            </div>
+          </label>
+        </v-form>
+      </v-card-text>
+      <v-divider />
+      <v-card-actions>
+        <v-spacer />
+        <v-btn text class="salmon--text" @click="cancel">Cancelar</v-btn>
+        <v-btn
+          class="white--text"
+          :class="{
+            'disable-events': !isFormFilled,
+            'salmon lighten-3': !isFormFilled,
+            'elevation-0': !isFormFilled,
+          }"
+          color="salmon"
+          rounded
+          width="72px"
+          @click="editContact"
+        >
+          Salvar
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+import { mapMutations, mapGetters } from 'vuex';
+
+import telephoneMask from '../utils/masks';
+
+export default {
+  name: 'CreateContactModal',
+  props: {
+    isVisible: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      contact: {
+        name: '',
+        email: '',
+        telephone: '',
+      },
+      valid: false,
+      telephoneMask,
+    };
+  },
+  methods: {
+    ...mapMutations(['EDIT_CONTACT']),
+    cancel() {
+      this.$emit('cancel');
+    },
+    isEmptyString(value) {
+      return value === '';
+    },
+    editContact() {
+      this.EDIT_CONTACT(this.contact);
+      this.$emit('close');
+    },
+  },
+  computed: {
+    ...mapGetters(['selectedContact']),
+    isFormFilled() {
+      const contactFormValues = Object.values(this.contact);
+      if (contactFormValues.every((this.isEmptyString))) {
+        return false;
+      }
+      return true;
+    },
+  },
+  watch: {
+    selectedContact(val) {
+      this.contact = { ...val };
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+  .contact-form {
+    padding: 24px;
+  }
+
+  .telephone-field {
+    width: 140px;
+  }
+
+  .disable-events {
+    pointer-events: none;
+  }
+</style>
